fd0d77d052707c97349c4eff1b3976b5bcbf1026
liuhao <liuhao@clounix.com>
2026-04-03 15:45:53 +0800

    1

--- config/appearance.lua
+++ config/appearance.lua
@@ -12,7 +12,7 @@ function M.apply(config)
     config.webgpu_power_preference = "HighPerformance"
 
     -- 窗口透明度
-    config.window_background_opacity = 0.9
+    config.window_background_opacity = 0.95
 
     -- 背景图片（如需启用请取消注释）
     -- config.window_background_image = constants.CONFIG_DIR .. "/images/4.jpg"
