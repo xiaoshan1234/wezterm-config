ba201ba823c5f0895ed773d0432f892d70a741eb
liuhao <liuhao@clounix.com>
2026-04-08 15:21:34 +0800

    launch

--- config/launch_menu.lua
+++ config/launch_menu.lua
@@ -3,14 +3,14 @@ local M = {}
 function M.apply(config)
     config.launch_menu = {
         -- Shell
-        { label = "Bash",           args = { "C:\\Program Files\\Git\\bin\\bash.exe", "-l" } },
-        { label = "PowerShell",     args = { "powershell.exe", "-NoLogo" } },
+        { label = "bash",           args = { "C:\\Program Files\\Git\\bin\\bash.exe", "-l" } },
+        { label = "powerShell",     args = { "powershell.exe", "-NoLogo" } },
 
         -- WSL 发行版
-        { label = "WSL: debian",    args = { "wsl.exe", "-d", "debian" } },
+        { label = "debian",    args = { "wsl.exe", "-d", "debian" } },
 
         -- SSH 连接
-        { label = "SSH: 10.18.0.20", args = { "ssh", "liuhao@10.18.0.20" } },
+        { label = "10.18.0.20", args = { "ssh", "liuhao@10.18.0.20" } },
     }
 end
 
